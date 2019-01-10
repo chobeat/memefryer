98e6e177c55ed8e8d4365af0998307a39b464801
Simone Robutti <simone.robutti@gmail.com>
2019-01-10 22:08:16 +0100

    replaced ImageMagick with pillow

--- src/memefryer/fryer.py
+++ src/memefryer/fryer.py
@@ -4,74 +4,136 @@ import face_recognition
 import os
 import random
 from shutil import copyfile
+from PIL import Image, ImageDraw, ImageEnhance, ImageFilter
+import numpy as np
+import colorsys
+import cv2 as cv
 
 
 class Fryer(object):
-    # improve the logic to decide the working file
-    WORKING_FILE = "/tmp/fried_image"
     def __init__(self):
 
         self.emojis_path = resource_filename("memefryer", "emoji")
-        self.emojis = list(filter(lambda x:".png" in x, resource_listdir("memefryer", "emoji")))
+        self.emojis = list(filter(lambda x: ".png" in x, resource_listdir("memefryer", "emoji")))
 
-    def fry_image(self, image_path, output_path):
-            copyfile(image_path,self.WORKING_FILE)
-            self._emojify()
-            self._color_filter()
-            copyfile(self.WORKING_FILE,output_path)
-            os.remove(self.WORKING_FILE)
+    def fry_from_file(self, image_path, output_path, random_transformations=False, with_emoji=True):
+        """
 
-    def _color_filter(self):
-        # It's really a mess of options, but it works.
-        call(["mogrify", "-contrast", "-contrast", "-level", "25%", "-modulate", "80,200", "-modulate", "100,150", "-level", "25%", "-contrast", "-contrast", self.WORKING_FILE])
+        :param image_path: input path of an image
+        :param output_path: output path where to save the result
+        :param random_transformations: applies some randomness to the result
+        :param with_emoji: replace faces with emojis
+        :return:
+        """
+        img = Image.open(image_path)
+        img = self.fry_image(img, random_transformations, with_emoji)
+        img.save(output_path)
 
-    def _emojify(self):
+    def fry_image(self,PIL_image, random_transformations=False, with_emoji=True):
+        """
+        Fries an image
+        :param PIL_image: a PIL image to be transformed
+        :param random_transformations: applies some randomness to the result
+        :param with_emoji: replace faces with emojis
+        :return: a fried PIL image
+        """
+        if with_emoji:
+            self._emojify(PIL_image)
+        return self._color_transformations(PIL_image, random_transformations)
+
+    def _color_transformations(self, img, random_transformations):
+        """
+        Apply all the transformations necessary to fry the image
+        :param img:
+        :param random_operations:
+        :return:
+        """
+        img = self._level_image(img)
+        basic_operations = [(ImageEnhance.Contrast, 0.9),
+                            (ImageEnhance.Color, 10.0),
+                            (ImageEnhance.Contrast, 0.9),
+                            (ImageEnhance.Sharpness, 15.0),]
+
+        other_operations = [
+            (ImageEnhance.Contrast, 0.9),
+            (ImageEnhance.Contrast, 1.2),
+            (ImageEnhance.Brightness, 1.25),
+            (ImageEnhance.Contrast, 0.9),
+            (ImageEnhance.Color, 5.0),
+        ]
+
+        if random_transformations:
+            random.shuffle(other_operations)
+
+        for operation, value in basic_operations+other_operations:
+            img = operation(img).enhance(value)
+
+
+        return img
+
+    def _emojify(self, img):
         """
         Detects faces and superimposes an emoji over them.
         Returns the path to the new picture, deleting the old one.
         """
-        faces = face_recognition.face_locations(face_recognition.load_image_file(self.WORKING_FILE))
+        faces = face_recognition.face_locations(np.array(img))
 
         if len(faces) == 0:
-            return self.WORKING_FILE
+            return img
 
-        imagemagick_cmd = ["convert", self.WORKING_FILE]
-
-        # compose imagemagick command to add multiple faces
         for face in faces:
-            imagemagick_cmd.extend(self._get_params_for_face(face))
+            self._replace_with_emoji(img, face)
 
-        output_path = self.WORKING_FILE + ".composite.jpg"
-
-        imagemagick_cmd.append(output_path)
-        call(imagemagick_cmd)
-
-        copyfile(output_path,self.WORKING_FILE)
-
-
-    def _get_params_for_face(self, face):
+    def _replace_with_emoji(self, img, face_position):
         """
         Takes a tuple for the face position, returns a piece of ImageMagick commands.
+
+        :param img:
+        :param face_position:
+        :return:
         """
-        (top, right, bottom, left) = face
-        # For debug purposes, draw a rectangle over the face
-        # draw_str = 'rectangle %d,%d %d,%d' % (left, top, right, bottom)
-        # return ["-fill", "green", "-stroke", "black", "-draw", draw_str]
-        emoji_path = self._pick_random_emoji()
+        (top, right, bottom, left) = face_position
+
         # Increase the size by 20%. Account for proper centering.
         scale_factor = 0.20
-        size_str = "%dx%d" % ((right - left) * (1 + scale_factor), (bottom - top) * (1 + scale_factor))
-        diff_x = (right - left) * (scale_factor / 2)
-        diff_y = (bottom - top) * (scale_factor / 2)
-        pos_str = "+%d+%d" % (left - diff_x, top - diff_y)
-        geometry_str = size_str + pos_str
-        return [emoji_path, "-geometry", geometry_str, "-composite"]
+        emoji_img = self.get_random_emoji_img(face_position, scale_factor)
+        diff_x = int((right - left) * (scale_factor / 2))
+        diff_y = int((bottom - top) * (scale_factor / 2))
 
+        img.paste(emoji_img, (left - diff_x, top - diff_y), mask=emoji_img)
+
+    def get_random_emoji_img(self, face_position, scale_factor):
+
+        (top, right, bottom, left) = face_position
+        emoji_path = self._pick_random_emoji()
+        emoji_img = Image.open(emoji_path)
+        size_x = int((right - left) * (1 + scale_factor))
+        size_y = int((bottom - top) * (1 + scale_factor))
+        return emoji_img.resize((size_x, size_y))
 
     def _pick_random_emoji(self):
         """
         Picks a random emoji among those available
         :return:
         """
-        position = random.randint(0, len(self.emojis)-1)
+        position = random.randint(0, len(self.emojis) - 1)
         return os.path.join(self.emojis_path, self.emojis[position])
+
+    def _level_image(self, img):
+        """
+        Shamelessly inspired by(copy-pasted from):
+        https://stackoverflow.com/questions/3105603/doing-the-same-as-imagemagicks-level-in-python-pil
+        :param img:
+        :return:
+        """
+        imgCV = np.asarray(img)
+        hsv = cv.cvtColor(imgCV, cv.COLOR_RGB2HSV)
+        h, s, v = cv.split(hsv)
+        ceil = np.percentile(v, 75)
+        floor = np.percentile(v, 25)
+        a = 255 / (ceil - floor)
+        b = floor * 255 / (floor - ceil)
+        v = np.maximum(0, np.minimum(255, v * a + b)).astype(np.uint8)
+        hsv = cv.merge((h, s, v))
+        rgb = cv.cvtColor(hsv, cv.COLOR_HSV2RGB)
+        return Image.fromarray(rgb)
--- tests/test_fry.py
+++ tests/test_fry.py
@@ -1,10 +1,19 @@
 from pkg_resources import resource_filename
 from memefryer.fryer import Fryer
 import os
+import pytest
 def test_e2e():
     file_dir=resource_filename("tests","resources")
     image_path=os.path.join(
         file_dir,"lenna.png"
     )
 
-    Fryer().fry_image(image_path,"/tmp/output.png")
+    Fryer().fry_from_file(image_path,"/tmp/output.png")
+
+def test_e2e_random():
+    file_dir=resource_filename("tests","resources")
+    image_path=os.path.join(
+        file_dir,"lenna.png"
+    )
+
+    Fryer().fry_from_file(image_path,"/tmp/output.png", random_transformations=True)
